37500ffa626a794572d4582fc0b35ec20e73042e
lzh <2212384795@qq.com>
2024-06-18 14:49:40 +0800

    添加酒店和机票销售金额跳转

--- src/components/my/MyInfo.vue
+++ src/components/my/MyInfo.vue
@@ -5,6 +5,7 @@
         <div class="user-profile">
           <el-avatar :size="100" :src="userInfo.user.avatar"></el-avatar>
           <h2>{{ userInfo.user.nickname }}</h2>
+          <el-button type="primary" @click="showProfileModal = true">修改个人信息</el-button>
         </div>
       </el-col>
       <el-col :span="21">
@@ -40,25 +41,54 @@
       </el-form-item>
     </form>
   </el-dialog>
+
+  <el-dialog
+      title="修改个人信息"
+      v-model="showProfileModal"
+      width="30%">
+    <form @submit.prevent="submitProfile">
+      <el-form-item label="昵称:">
+        <el-input v-model="editProfileForm.nickname"></el-input>
+      </el-form-item>
+      <el-form-item label="头像:">
+        <el-upload
+            class="avatar-uploader"
+            action=""
+            :show-file-list="false"
+            :before-upload="beforeAvatarUpload"
+            :auto-upload="false"
+            :on-change="handleAvatarChange">
+          <img v-if="editProfileForm.avatar" :src="editProfileForm.avatar" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">提交</el-button>
+      </el-form-item>
+    </form>
+  </el-dialog>
 </template>
 
 <script setup>
-import { ElContainer, ElRow, ElCol, ElAvatar, ElButton, ElTable, ElTableColumn } from 'element-plus';
+import { ElContainer, ElRow, ElCol, ElAvatar, ElButton, ElTable, ElTableColumn, ElUpload } from 'element-plus';
 import { computed, onMounted, ref } from 'vue';
 import { useUserStore } from '../../stores/userStore';
-import {add} from "../../api/login";
-import {fetchContactPeople6} from "../../api/hotel";
+import { add, updatePro
+  file } from "../../api/login";
+import { fetchContactPeople6 } from "../../api/hotel";
 
 const userStore = useUserStore();
 const userInfo = ref({
   user: {},
-  contacts: [
-  ]
+  contacts: []
 });
 const showModal = ref(false);
+const showProfileModal = ref(false);
 const addContactForm = ref({ name: '', identityCardNumber: '', phoneNumber: '' });
-
+const editProfileForm = ref({ nickname: '', avatar: '' });
+const selectedFile = ref(null);
 const userInfo1 = computed(() => userStore.userInfo);
+
 
 const submitContact = async () => {
   console.log('提交联系人:', addContactForm.value);
@@ -67,15 +97,15 @@ const submitContact = async () => {
     phoneNumber: addContactForm.value.phoneNumber,
     identityCardNumber: addContactForm.value.identityCardNumber
   };
-  console.log('newContact',newContact)
+  console.log('newContact', newContact)
   userInfo.value.contacts.push(newContact);
 
-  console.log('userInfo.value.contacts',userInfo.value.contacts)
+  console.log('userInfo.value.contacts', userInfo.value.contacts)
 
   try {
     const userId = userInfo.value.user.id;
-    const respone = await add(newContact, userId);
-    console.log(respone)
+    const response = await add(newContact, userId);
+    console.log(response)
   } catch (error) {
     console.error('Error saving contact:', error);
   }
@@ -84,12 +114,56 @@ const submitContact = async () => {
   showModal.value = false;
 };
 
+const submitProfile = async () => {
+  console.log('提交个人信息:', editProfileForm.value);
+  try {
+    const userId = userInfo.value.user.id;
+    const formData = new FormData();
+    formData.append('file', selectedFile.value);
+    formData.append('nickname', editProfileForm.value.nickname);
+
+    const response = await updateProfile(userId, formData);
+
+    console.log(response);
+    userInfo.value.user.nickname = editProfileForm.value.nickname;
+    userInfo.value.user.avatar = response.data;
+  } catch (error) {
+    console.error('更新失败:', error);
+  }
+
+  showProfileModal.value = false;
+};
+const handleAvatarChange = (file) => {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    editProfileForm.value.avatar = e.target.result;
+  };
+  reader.readAsDataURL(file.raw);
+  selectedFile.value = file.raw;
+};
+const beforeAvatarUpload = (file) => {
+  const isImage = file.type.startsWith('image/jpeg');
+  const isLt2M = file.size / 1024 / 1024 < 10;
+
+  if (!isImage) {
+    this.$message.error('上传头像图片只能是图片格式!');
+  }
+  if (!isLt2M) {
+    this.$message.error('上传头像图片大小不能超过 10MB!');
+  }
+  return isImage && isLt2M;
+};
+
+
+
 onMounted(async () => {
   userInfo.value.user = userInfo1.value;
+  editProfileForm.value.nickname = userInfo.value.user.nickname;
+  editProfileForm.value.avatar = userInfo.value.user.avatar;
 
   const response = await fetchContactPeople6(userInfo.value.user.id);
 
-  console.log('哈哈哈哈哈',userInfo.value.user.id)
+  console.log('哈哈哈哈哈', userInfo.value.user.id)
   userInfo.value.contacts = response.data;
 
   userInfo.value.contacts = [...new Map(userInfo.value.contacts.concat(response.data).map(item => [item['contactId'], item])).values()];
@@ -105,5 +179,20 @@ onMounted(async () => {
 .contacts {
   padding: 20px;
 }
-
+.avatar-uploader {
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+}
+.avatar {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  line-height: 100px;
+  text-align: center;
+}
 </style>
